a31e47adb63b3ab4e40a7f74b6da3881057a1e72
Noam Elfanbaum <noam.elf@gmail.com>
2018-06-18 01:33:52 +0300

    Support readme

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md')) as f:
 
 setup(
     name='strictclasses',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/noamelf/strictclasses',
     author='Noam Elfanbaum',
     author_email='noam.elf@gmail.com',
