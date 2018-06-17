be62b04bbd05762db4a1a99f867add8341a8d5fb
Noam Elfanbaum <noam.elf@gmail.com>
2018-06-18 01:30:29 +0300

    Add readme to pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
+from os import path
+
 from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='strictclasses',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/noamelf/strictclasses',
     author='Noam Elfanbaum',
     author_email='noam.elf@gmail.com',
     description='A strict companion to dataclasses',
     packages=['strictclasses'],
     install_requires=['dataclasses'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
