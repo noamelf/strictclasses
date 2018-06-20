d8bac3ca260df1ed9d7a4a264c137eb5016ccac7
lior <li.mizr@gmail.com>
2018-06-20 19:37:37 +0300

    review

--- strictclasses/__init__.py
+++ strictclasses/__init__.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for strictclass__cookie."""
+from .strictclass import strict, strictclass
 
 __author__ = """Noam Elfanbaum"""
 __email__ = 'noam.elf@gmail.com'
 __version__ = '0.1.0'
-
-from .strictclass import strict, strict_dataclass
